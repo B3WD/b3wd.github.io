b674e8db2679748e6e22fb6af55061f52cafe6a8
B3WD <tsvetomir.kpavlov@gmail.com>
2025-08-30 13:35:14 +0300

    Give each button a unique id

--- components/rng-bg-btn.js
+++ components/rng-bg-btn.js
@@ -1,12 +1,14 @@
 import { generateRandomColor } from "../scripts/random_background.js";
 
-
 class RngBgBtn extends HTMLElement {
+    static btnCount = 0;
+
     connectedCallback() {
-        const buttonId = "random";
+        const buttonId = `random-${RngBgBtn.btnCount++}`;
         this.innerHTML = `<button id="${buttonId}">Random</button>`;
-        document.querySelector(`#${buttonId}`).onclick = function () {
+        this.querySelector(`#${buttonId}`).onclick = function () {
             document.body.style.backgroundColor = generateRandomColor();
+            console.log('Background color changed to:', document.body.style.backgroundColor);
         };
     }
 }
