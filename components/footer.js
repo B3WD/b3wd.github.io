88f0961a843a9d30b52708ccc9d991d02f0d375a
B3WD <tsvetomir.kpavlov@gmail.com>
2025-08-30 13:23:18 +0300

    Reusable and composable components with bundled logic

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,21 @@
 class ReusableFooter extends HTMLElement {
     connectedCallback() {
+        // Dynamically load rng-bg-btn.js
+        const script = document.createElement('script');
+        script.src = '../components/rng-bg-btn.js';
+        script.type = 'module';
+        document.head.appendChild(script);
+
         this.innerHTML = `
-        <footer>
-            <div></div>
+        <footer class="fsmall">
+            <div>
+                Have fun with the background color!
+            </div>
+            <div>
+                <rng-bg-btn></rng-bg-btn>
+            </div>
         </footer>
-        `
+        `;
     }
 }
 
---
+++ components/rng-bg-btn.js
@@ -0,0 +1,14 @@
+import { generateRandomColor } from "../scripts/random_background.js";
+
+
+class RngBgBtn extends HTMLElement {
+    connectedCallback() {
+        const buttonId = "random";
+        this.innerHTML = `<button id="${buttonId}">Random</button>`;
+        document.querySelector(`#${buttonId}`).onclick = function () {
+            document.body.style.backgroundColor = generateRandomColor();
+        };
+    }
+}
+
+customElements.define("rng-bg-btn", RngBgBtn)
