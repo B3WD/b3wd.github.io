6806f33ea33907919910a5fd4f1dc1f647474239
B3WD <tsvetomir.kpavlov@gmail.com>
2025-09-05 09:53:12 +0300

    Implement dynamic article loading; Add enchanced multi-column library;

--- components/article_card.js
+++ components/article_card.js
@@ -1,12 +1,12 @@
 function articleCard(imgSrc, imgAlt, title, text, linkHref, linkText) {
     return `
-        <div class="article-card" style="width: 18rem;">
-            <img src="${imgSrc}" class="card-img-top" alt="${imgAlt}">
-            <div class="article-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${text}</p>
-                <a href="${linkHref}" class="btn btn-primary">${linkText}</a>
-            </div>
+    <div class="article-card">
+        <img src="${imgSrc}" class="card-img-top" alt="${imgAlt}">
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${text}</p>
+            <a href="${linkHref}" class="btn btn-primary">${linkText}</a>
         </div>
-    `;
+    </div>
+`;
 }
